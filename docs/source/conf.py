899b570cdbe5bacf5d737caba3db4a1ec18f82ff
Claas <claas.rostock@dnv.com>
2025-12-09 20:35:28 +0100

    bumped version number to 0.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = "2025, Claas Rostock"
 author = "Claas Rostock"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.2.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
